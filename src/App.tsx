63b71136385ab728c3b6a1826bfaf8795425d4fb
Xena <zakgriffin@Zaks-Awesome-Piece-of-Magical-Hardware.local>
2020-05-03 17:23:04 -0400

    really slow and coded badly, but things kinda work

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ToolGrid, { ToolGridData, initTools} from './components/ToolGrid'
 import PackUpload from './components/PackUpload'
 import PixelDisplay from './components/PixelDisplay'
 import { PixelImage } from './PixelLogicInterfaces'
+import ErodeViewer from './components/ErodeViewer'
 
 export default function App() {
     const [tools, setTools] = useState<ToolGridData>(initTools)
@@ -12,7 +13,8 @@ export default function App() {
         <ToolGrid {...{tools, setCurrentTool}}/>
         <PackUpload {...{tools, setTools}}/>
         <div>
-            <PixelDisplay imageData={currentTool}/>
+            <PixelDisplay imageData={currentTool} style={{width: 400}}/>
         </div>
+        <ErodeViewer currentImage={currentTool}/>
     </>
 }
---
+++ src/components/ErodeViewer.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import { PixelImage } from '../PixelLogicInterfaces'
+import PixelDisplay from './PixelDisplay'
+import { erode } from '../PixelLogic'
+
+interface ErodeViewerProps {
+    currentImage: PixelImage | undefined
+}
+
+export default function ErodeViewer({currentImage}: ErodeViewerProps) {
+    const [erodeStages, setErodeStages] = useState<(PixelImage | undefined)[]>([undefined])
+
+    useEffect(() => {
+        setErodeStages(Array(erodeStages.length).fill(currentImage))
+    }, [currentImage])
+
+    const addErodeStage = () => {
+        setErodeStages([...erodeStages, undefined])
+    }
+    const removeErodeStage = () => {
+        setErodeStages(erodeStages.slice(1))
+    }
+    const erodeAll = () => {
+        let first = erodeStages[0]
+        if(!first) return
+        let newEroded = [first]
+
+        for(let i = 1; i <= erodeStages.length; i++) {
+            let toErode = newEroded[i - 1]
+            if(!toErode) break
+            let eroded = erode(toErode, {
+                areaOfInfluence: 3,
+                percentToRemain: 1,
+                //carryEdgeColor: true,
+                randomSurviveChance: 0.9,
+                circular: true
+            })
+            newEroded[i] = eroded
+        }
+        console.log(newEroded)
+        setErodeStages(newEroded)
+    }
+
+    return <>
+        {
+            erodeStages?.map((erodeStage, i) => 
+                <PixelDisplay key={i} imageData={erodeStage} style={{border: 'red 1px solid', width: 150}}/>
+            )
+        }
+        <button
+            onClick={addErodeStage}
+        >Add Erode Stage</button>
+        <button
+            onClick={removeErodeStage}
+        >Remove Erode Stage</button>
+        <button
+            onClick={erodeAll}
+        >Erode ALL</button>
+    </>
+}
